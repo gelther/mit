20fe4cd0f6fb98142e6cbd917594a0eb1d8c77a8
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-19 18:31:09 +0200

    Code consistency changes for frontend/views/tracking-universal.php

--- frontend/views/tracking-universal.php
+++ frontend/views/tracking-universal.php
@@ -16,10 +16,10 @@
 if ( count( $gaq_push ) >= 1 ) {
 	foreach ( $gaq_push as $item ) {
 		if ( ! is_array( $item ) ) {
-			echo '	__gaTracker('.$item.");\n";
+			echo '	__gaTracker(' . $item . ");\n";
 		}
 		elseif ( isset( $item['value'] ) ) {
-			echo '	'.$item['value'] . "\n";
+			echo '	' . $item['value'] . "\n";
 		}
 	}
 }
