e203b751871659e6b267d801bb316e0314199c7f
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-19 18:30:16 +0200

    Code consistency changes for admin/class-google-analytics.php

--- admin/class-google-analytics.php
+++ admin/class-google-analytics.php
@@ -30,10 +30,8 @@ class Yoast_Google_Analytics {
 
 	/**
 	 * Singleton
-	 *
 	 */
 	protected function __construct() {
-
 		if ( is_null( self::$instance ) ) {
 			self::$instance = $this;
 		}
@@ -65,7 +63,6 @@ class Yoast_Google_Analytics {
 	 * Check if something went wrong with API calls to Google Analytics
 	 */
 	public function check_for_ga_issues() {
-
 		$last_run   = get_option( 'yst_ga_last_wp_run' );
 		$has_failed = get_option( 'yst_ga_api_call_fail', false );
 
@@ -80,13 +77,12 @@ class Yoast_Google_Analytics {
 
 			add_action( 'admin_notices', array( 'Yoast_Google_Analytics_Notice', $notice_type ) );
 		}
-
 	}
 
 	/**
 	 * Wrapper for authenticate the client. If authentication code is send it will get and check an access token.
 	 *
-	 * @param mixed $authentication_code
+	 * @param  mixed   $authentication_code
 	 *
 	 * @return boolean
 	 */
@@ -124,12 +120,11 @@ class Yoast_Google_Analytics {
 	 *
 	 * This method will do a request to google and get the response code and body from content
 	 *
-	 * @param string $target_request_url
+	 * @param  string     $target_request_url
 	 *
 	 * @return array|null
 	 */
 	public function do_request( $target_request_url ) {
-
 		$response = $this->client->do_request( $target_request_url );
 
 		if ( ! empty( $response ) ) {
@@ -138,7 +133,6 @@ class Yoast_Google_Analytics {
 				'body'     => json_decode( $response->getResponseBody(), true ),
 			);
 		}
-
 	}
 
 
@@ -248,12 +242,11 @@ class Yoast_Google_Analytics {
 	/**
 	 * Format the accounts request
 	 *
-	 * @param array $response
+	 * @param  array $response
 	 *
 	 * @return mixed
 	 */
 	private function format_profile_call( $response ) {
-
 		if ( isset( $response['response']['code'] ) && $response['response']['code'] == 200 ) {
 			if ( ! empty( $response['body']['items'] ) && is_array( $response['body']['items'] ) ) {
 				$accounts = array();
